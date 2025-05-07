d8c08b9f5d215a4ac5f125ab0aa43854e12eb809
harryli0088 <harryli0088@gmail.com>
2025-05-06 20:07:18 -0400

    converted head and footer

---
+++ src/components/Head.tsx
@@ -0,0 +1,25 @@
+export default function Head() {
+  return (
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>Welcome to IEEE VIS 2025</title>
+
+      {/* <!-- favicon --> */}
+      <link rel="shortcut icon" href="/assets/favicon.png" type="image/x-icon" />
+
+      {/* <!-- Include Material Icons --> */}
+      <link
+          href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet"
+      />
+
+      {/* <!-- Include fonts --> */}
+      <link
+          href="https://fonts.googleapis.com/css?family=Fira+Sans:400,700|Zilla+Slab:400,500,700&display=swap"
+          rel="stylesheet"
+      />
+    </head>
+  )
+}
