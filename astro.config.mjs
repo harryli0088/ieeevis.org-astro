1392bf593f80bb859df7b6b2ad152197f2a9b202
harryli0088 <harryli0088@gmail.com>
2025-12-08 16:53:05 -0500

    set base url for relative improts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  base: `/year/${process.env.DEPLOY_YEAR}`,
   integrations: [react()],
-
   vite: {
     plugins: [tailwindcss()]
   }
