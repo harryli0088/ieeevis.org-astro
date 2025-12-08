5fb5f70e6d926e24021749147a391b0f068c0700
harryli0088 <harryli0088@gmail.com>
2025-12-08 17:20:30 -0500

    updated config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,18 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+let base = "/";
+if(process.env.NODE_ENV === "production") {
+  if(!process.env.DEPLOY_YEAR) {
+    throw new Error("You must specify a DEPLOY_YEAR environment variable");
+  }
+  base = `/year/${process.env.DEPLOY_YEAR}`;
+  console.log("BASE",base)
+}
+
 // https://astro.build/config
 export default defineConfig({
+  base,
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()]
